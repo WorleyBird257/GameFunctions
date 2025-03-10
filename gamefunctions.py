d6f4c9bac6f7db10c7d5b43c40c2c1f6ce567d43
WorleyBird257 <worley.jane710@gmail.com>
2025-03-09 21:44:03 -0600

    gamefunctions.py
    3/9 updates. formatting, doc and function strings added.

--- gamefunctions.py
+++ gamefunctions.py
@@ -1,48 +1,62 @@
+'''
+Actionable functions for a game.
+
+Four functions are present:
+-purchase_item(itemPrice, startingMoney, quantityToPurchase)
+-new_random_monster(monster_name)
+-print_welcome(name)
+-print_shop_menu(item1Name, item1Price, item2Name, item2Price)
+'''
+
 # Adventure Functions
-# 2/15/25, 3/1/25
 # Reya Worley
 # gamefunctions.py
 
-import random
-
-# all variables are randomized
-
 #################################################################
-#      purchase Item function
+
+import random # all variables are randomized for this function
 
 itemPrice = float(random.randint(1, 100))
 startingMoney = float(random.randint(1, 1000))
 quantityToPurchase = int(random.randint(1, 100))
 
 def purchase_item(itemPrice, startingMoney, quantityToPurchase):
-    ''' function purchases items without spending more money than available
-    Three parameters: itemPrice, startingMoney, quanitityToPurchase
-    -limits quantity to available purchasing power
-    -default value = 1
-    -shows correct change after purchase'''
-    maxItems = int(startingMoney // itemPrice)  # max allowed items to purchase 
+    '''
+    Purchases items without spending more money than available.
+
+    Parameters:
+        itemPrice (float)
+        startingMoney (float)
+        quanitityToPurchase (int)
+
+    Returns:
+        remaining_money, purchasedQuantity
+    '''
+    
+    maxItems = startingMoney // itemPrice  # max allowed items to purchase 
     purchasedQuantity = min(quantityToPurchase, maxItems)  # limits items purchased (no negative integers)
     purchase_value = itemPrice * purchasedQuantity  # calculates the value based on purchasedQuantity
     remaining_money = startingMoney - purchase_value  # calculates the difference between starting money and what's left
     
-    return f'Items purchased: {purchasedQuantity}\nRemaining money: {remaining_money}\n'
-
-#Test Code
-if __name__ in '__main__':
-    print(purchase_item(1.58, 100, 3))
-    print(purchase_item(itemPrice, startingMoney, quantityToPurchase))
-    print(purchase_item(itemPrice, startingMoney, quantityToPurchase=1)) #default quantity
+    return purchasedQuantity, remaining_money
 
 ##################################################################
 
-# Create a random Monster:
-#output is the profile of a monster based on 5 options with random integers for health, power, and money. 
+def new_random_monster(monster_name=None): 
+    '''
+    Creates a monster with randomized health, power, and money values associated to name and a brief description.
 
-def new_random_monster(monster_name=None): #first dictionary holds name and values for "stats"
-    ''' Creates monster with random values.
-    Monster has 'health, power, and money' associated to name.
-    Monstere includes a brief description. '''
-    monster_values_dict = {
+    Parameters:
+        monster_name (str)
+
+    Returns:
+        monster_values (dict)
+
+    Example:
+        >>> new_random_monster('Goblin')
+    '''
+    
+    monster_values_dict = { #first dictionary holds name and values for "stats"
         'Goblin': {'health': random.randint(140, 160), 'power': random.randint(15, 30), 'money': random.randint(1, 10)},
         'Draugr': {'health': random.randint(140, 175), 'power': random.randint(15, 25), 'money': random.randint(1, 15)},
         'Bat': {'health': random.randint(50, 80), 'power': random.randint(10, 25), 'money': random.randint(1, 7)},
@@ -71,54 +85,48 @@ def new_random_monster(monster_name=None): #first dictionary holds name and valu
         'money': monster_values['money']
         }
 
-# Example usage:
-print(new_random_monster())
-
-# example to show the same monster name but with different properties
-monster_name = 'Goblin'
-monster1 = new_random_monster(monster_name)
-monster2 = new_random_monster(monster_name)
-
-print(monster1)
-print(monster2)
-
-########################################################################
-########################################################################
-#Assignment 6
-
-
-#   Welcome!
+#########################################################
+ 
 def print_welcome(name='Reya'): 
-    '''prints welcome with 'name' parameter
-    output is centered in 20 character field'''
-    return f'{'Welcome, ' + name:^20}'
+    '''
+    Prints a welcome banner where a given name is centered in a 20 character field.
+    
+    Parameters:
+        name
 
-if __name__ in '__main__':
-    print(print_welcome()) #4
-    print(print_welcome(name='Gerald')) #6
-    print(print_welcome('Guinnivere')) #10
+    Returns:
+        None
+    '''
+    nameLength = len(name)
+    #fieldWidth = 20 - nameLength #keeps uniform width with various length name
+    return f'{' Welcome, ' + name:^20}'
 
 ##########################################################
-#   Formatted shop sign, 
+
 def print_shop_menu(item1Name='Potion', item1Price=50, item2Name='Elixir', item2Price=75): #my god, it's even got a border 
-    '''prints sign with list of two items and corresponding prices
-    item name field is 12 characters, item price field is 8 characters
-    prices have 2 decimal places with dollar sign ie) $12.34
-    parameters: item1Name, item1Price, item2Name, item2Price
+    '''
+    Prints a sign from a list of two items with their corresponding prices.
+
+    Item name field is 12 characters, item price field is 8 characters
+    and prices have 2 decimal places with dollar sign ie) $12.34.
+    
+    Parameters:
+        item1Name
+        item1Price
+        item2Name
+        item2Price
     '''
 
     itemWidth = 12
-
-    item1PriceWidth = len(str(item1Price))
-    priceWidth = abs(5-item1PriceWidth)
+    pricePrecision = 2
+    priceWidth = 8
     
-
+    item1PriceWidth = len(str(item1Price))
+    priceWidth = abs(item1PriceWidth - pricePrecision)
+    
     item2PriceWidth = len(str(item2Price))
     priceWidthBuffer = abs(5- item2PriceWidth)
     priceWidth = item2PriceWidth + (4 - priceWidthBuffer)
-    
-    pricePrecision = 2
-
 
     print(' ' + '_' * 18 + ' ')
     print('|' + '*' + ' ' * 16 + '*' + '|')
@@ -126,8 +134,7 @@ def print_shop_menu(item1Name='Potion', item1Price=50, item2Name='Elixir', item2
     print('|' + f'{item2Name:<{itemWidth}}' + f'{'$':>}{item2Price:>{priceWidth}.{pricePrecision}f}' + '|')
     print('|' + ' ' * 18 + '|')
     print('|' + '_' * 18 + '|')
-
-
+    
 # Insane debugging. still doesn't work for price values of 5 index. 
     #print(len((' ' + '_' * 18 + ' ')))
     #print(len(('|' + '*' + ' ' * 16 + '*' + '|')))
@@ -139,21 +146,38 @@ def print_shop_menu(item1Name='Potion', item1Price=50, item2Name='Elixir', item2
     #print(item1PriceWidth)
     #print(item2PriceWidth)
     #print(priceWidthBuffer)
-    
           
 item1Name = str()
 item1Price = float()
 item2Name = str()
 item2Price = float()
-if __name__ in '__main__':
-    print_shop_menu()
-    print_shop_menu(item1Name, item1Price, item2Name, item2Price)
-    print_shop_menu('Apple', 31.5, 'Orange', 3.2)
-
 
 #limitations of the formatting, changing the "size" of the price
 # changes the width of the formatting.
 # can't figure out how to have '$' stay consistent
 
+##################################################################
 
+#Test Code #
+if __name__ in '__main__': #purchase_item
+    def test_functions():
+        print(purchase_item(1.58, 100, 3))
+        print(purchase_item(itemPrice, startingMoney, quantityToPurchase))
+        print(purchase_item(itemPrice, startingMoney, quantityToPurchase=1)) #default quantity
 
+        print(new_random_monster()) #new_random_monster
+        monster_name = 'Goblin' # same monster name but with different properties
+        monster1 = new_random_monster(monster_name)
+        monster2 = new_random_monster(monster_name)
+        print(monster1)
+        print(monster2)
+
+        print(print_welcome()) #4 #welcome, with different names
+        print(print_welcome(name='Gerald')) #6
+        print(print_welcome('Guinnivere')) #10
+
+        print_shop_menu()
+        print_shop_menu(item1Name, item1Price, item2Name, item2Price)
+        print_shop_menu('Apple', 31.5, 'Orange', 3.2)
+
+    print(test_functions())
